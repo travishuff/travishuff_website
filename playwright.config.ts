39306d17f6d8863d5c6fd4027001f8f2b3bb4d96
Travis Huff <huff.travis@gmail.com>
2026-06-04 21:50:56 -0700

    Switch site runtime to Bun

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev',
+    command: 'bun run dev',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
   },
